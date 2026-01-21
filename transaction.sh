6cd79dd211e50c29cab9eb9f8b442b1c37de7e84
easy2learning4-source <root@gitmachine.3m232lqzushunpfaejt15c0wdb.oslx.internal.cloudapp.net>
2026-01-21 13:41:51 +0000

    ticket 7272727 adding support file

--- transaction.sh
+++ transaction.sh
@@ -2,3 +2,5 @@ hi sh
 1. soft reset: Soft reset will move the HEAD pointer to particular commit and record changes. In this process of moving HEAD, soft reset will not change anything in working directory or staging area, but it will move changes from local repository.
 2. mixed reset: Mixed reset will not changes anything in working directory ,but it will move changes from staging area and local repository.
 3. hard reset: Hard reset will move changes from all stages of git to particular commit i.e Hard reset will move the HEAD to particular commit to record changes from that point.
+hello
+
